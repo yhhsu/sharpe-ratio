c76dc1a81801c7cf86eeccc5f282a0be4ff14049
yhhsu <55427893+yhhsu@users.noreply.github.com>
2022-01-04 14:29:07 +0800

    Update maritime_industry.py

--- maritime_industry.py
+++ maritime_industry.py
@@ -10,83 +10,71 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 %matplotlib inline
 
+# Evergreen
 evergreen_df = yf.download('2603.TW', 
                       start='2021-01-01', 
                       end='2021-12-31', 
-                      progress=False,
-)
+                      progress=False)
 
 evergreen = evergreen_df['Close']
 evergreen.describe()
 
+# Yang Ming
 yangming_df = yf.download('2609.TW', 
                       start='2021-01-01', 
                       end='2021-12-31', 
-                      progress=False,
-)
+                      progress=False)
+
 yangming = yangming_df['Close']
 yangming.describe()
 
+# Wan Hai
 wanhai_df = yf.download('2615.TW', 
                       start='2021-01-01', 
                       end='2021-12-31', 
-                      progress=False,
-)
+                      progress=False)
+
 wanhai = wanhai_df['Close']
 wanhai.describe()
 
+# Combine the 3 dataframe
 ship_df = pd.concat([evergreen.rename('Evergreen'), yangming.rename('Yang Ming'), wanhai.rename('Wan Hai')], axis=1)
 ship_df
 
+# TAIEX Index
 tw_df = yf.download('^TWII', 
                       start='2021-01-01', 
                       end='2021-12-31', 
-                      progress=False,
-)
+                      progress=False)
+
 tw = tw_df['Close']
 tw
 
-ship_df.plot(subplots = True, title = 'Stock Data')
 
+ship_df.plot(subplots = True, title = 'Stock Data')
 tw.plot(title = 'TAIEX')
 
 # calculate daily stock_data returns
 ship_returns = ship_df.pct_change()
-
-# plot the daily returns
 ship_returns.plot()
-
-# summarize the daily returns
 ship_returns.describe()
 
 # calculate daily benchmark_data returns
 tw_returns = tw.pct_change()
-
-# plot the daily returns
 tw_returns.plot()
-
-# summarize the daily returns
 tw_returns.describe()
 
 # calculate the difference in daily returns
 excess_returns = ship_returns.sub(tw_returns, axis = 0)
-
-# plot the excess_returns
 excess_returns.plot()
-
-# summarize the excess_returns
 excess_returns.describe()
 
 # calculate the mean of excess_returns 
 avg_excess_return = excess_returns.mean()
-
-# plot avg_excess_returns
 avg_excess_return.plot.bar(title = 'Mean of the Return Difference')
 
 # calculate the standard deviations
 sd_excess_return = excess_returns.std()
-
-# plot the standard deviations
 sd_excess_return.plot.bar(title = 'Standard Deviation of the Return Difference')
 
 # calculate the daily sharpe ratio
